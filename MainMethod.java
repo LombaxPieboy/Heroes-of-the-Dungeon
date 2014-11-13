fc3712631b555ec796505f7fbf1713725bd56c63
LombaxPieboy <Brandon.Ogon@gmail.com>
2014-11-12 20:04:47 -0400

    Start of the MainMethod

---
+++ MainMethod.java
@@ -0,0 +1,16 @@
+/**MainMethod for Heroes of The Dungeon
+	@author Brandon Ogon
+	@co-author Nick Tremblay
+	@date November 12, 2014
+*/
+
+public class MainMethod {
+public static void main(String[] args){
+
+
+
+
+
+
+	}
+}
